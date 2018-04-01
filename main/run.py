20c9abacfe0cb25147e04866afd7cb461deafae8
snappy45 <musanje2010@gmail.com>
2018-04-01 10:57:08 +0300

    added migrations, created a database file, restructured swagger docs, modified config and added them into the entry point of the application [Fixes #156246439]

--- main/run.py
+++ main/run.py
@@ -1,24 +1,28 @@
 from flask import Flask, Blueprint
-from config import Config, config
+# from config import Config, config
 from appModels import db
 from flasgger import Swagger
+from flasgger import swag_from
 import sys
+from user.userViews import userBlueprint
 
-userBlueprint = Blueprint('user', __name__)
-businessBlueprint = Blueprint('business', __name__)
-reviewBlueprint = Blueprint('review', __name__)
+# userBlueprint = Blueprint('user', __name__)
+# businessBlueprint = Blueprint('business', __name__)
+# reviewBlueprint = Blueprint('review', __name__)
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
 #Registering the blueprints for the different views
 app.register_blueprint(userBlueprint)
-app.register_blueprint(businessBlueprint)
-app.register_blueprint(reviewBlueprint)
+# app.register_blueprint(businessBlueprint)
+# app.register_blueprint(reviewBlueprint)
+
+
 
 #lets set the configurations from the config.py file
-app.config.from_object(Config)
-app.config['SQLALCHEMY_DATABASE_URI'] = config['default']
+# app.config.from_object(Config)
+# app.config['SQLALCHEMY_DATABASE_URI'] =
 
 # db.init_app(app)
 
--- main/user/userViews.py
+++ main/user/userViews.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Flask, request, json, jsonify, make_response
+import random
 import jwt
 import datetime
 import sys
@@ -6,34 +7,34 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flasgger import swag_from
 
 sys.path.append('..')
-from ..authourization.auth import token_required
-from ..run import userBlueprint
-# userBlueprint = Blueprint('user', __name__)
+# from ..authourization.auth import token_required
+# from ..run import userBlueprint
 
+userBlueprint = Blueprint('user', __name__)
 
 @userBlueprint.route('/api/auth/register', methods=['POST'])
-@swag_from('userSwagDocs/createUser.yml')
+@swag_from('createUser.yml')
 def createuser():
-    pass
+    return jsonify({'message':'just testing'})
         
 
-@userBlueprint.route('/api/auth/login', methods=['POST'])
-@swag_from('userSwagDocs/loginUser.yml')
-def login():
-    pass
+# @userBlueprint.route('/api/auth/login', methods=['POST'])
+# @swag_from('..swagger/loginUser.yml')
+# def login():
+#     pass
     
 
-@userBlueprint.route('/api/v1/auth/resetpassword', methods=['POST'])
-@swag_from('userSwagDocs/resetUserPassword.yml')
-@token_required
-def resetPassword():
-    pass
+# @userBlueprint.route('/api/v1/auth/resetpassword', methods=['POST'])
+# @swag_from('..swagger/resetUserPassword.yml')
+# # @token_required
+# def resetPassword():
+#     pass
 
-@userBlueprint.route('/api/v1/auth/logout', methods=['PUT'])
-@swag_from('userSwagDocs/logoutUser.yml')
-@token_required
-def logout():    
-    pass
+# @userBlueprint.route('/api/v1/auth/logout', methods=['PUT'])
+# @swag_from('userSwagDocs/logoutUser.yml')
+# # @token_required
+# def logout():    
+#     pass
 
 
 
